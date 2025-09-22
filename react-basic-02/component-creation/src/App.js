8f1b8b92b424491b142270de84a2838a5d0be63e
Gauri <gauripatil2999@gmail.com>
2025-09-22 12:25:47 +0530

    React.js: Component creation

---
+++ react-basic-02/component-creation/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+
+function ProductCard() {
+  return (
+    <article>
+      <h2>iPhone 15 Pro</h2>
+      <img 
+        src="images/iphone.jpg"
+        alt="iPhone 15 Pro"
+        width="128px"
+        height="128px"
+      />
+      <p>Specification:</p>
+      <ul>
+        <li>A17 Pro chip with 6-core GPU</li>
+        <li>3x or 5x Telephoto camera</li>
+        <li>Up to 29 hours video playback</li>
+      </ul>
+
+      <button>Buy (from $999)</button>
+      </article>    
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <ProductCard/>
+    </div>
+  );
+}
+
+export default App;
