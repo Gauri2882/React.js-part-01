b32551441ee00b2b6382f18736a03dea33c0035d
Gauri <gauripatil2999@gmail.com>
2025-09-25 11:11:49 +0530

    React.js: Component Props Manipulation

---
+++ react-basic-03/component-props-manipulation/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import { ProductCard } from './components/ProductCard';
+import { ProductList } from './components/ProductList';
+
+function App() {
+  const product = {
+        imageSrc: "images/iphone.jpg",
+        title: "iPhone 15 Pro",
+        specification: [
+            "A17 Pro chip with 6-core GPU",
+            "3x or 5x Telephoto camera",
+            "Up to 29 hours video playback"
+        ],
+        price: 999
+    };
+
+  return (
+    <div className="App">
+      <ProductList>
+        <ProductCard background = "darkolivegreen"product={product}/>
+        <ProductCard product={product}/>
+        <ProductCard background = "peru" product={product}/>
+      </ProductList>
+    </div>
+  );
+}
+
+export default App;
